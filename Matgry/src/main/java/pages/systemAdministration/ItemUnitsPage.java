47485ac2c2cbb9dedad8159b502d8325077e67e0
Ahmed Ghaly <a.ahmed@orobasoft.com>
2025-03-06 12:58:50 +0200

    [Finish all the CRUD OP on Item Units Module (Page and Tests)

--- Matgry/src/main/java/pages/systemAdministration/ItemUnitsPage.java
+++ Matgry/src/main/java/pages/systemAdministration/ItemUnitsPage.java
@@ -1,4 +1,88 @@
 package pages.systemAdministration;
 
+import driverFactory.Driver;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import pages.Commons;
+
 public class ItemUnitsPage {
+    public Driver driver;
+    public Commons commons;
+
+    public ItemUnitsPage(Driver driver) {
+        this.driver = driver;
+        commons = new Commons(driver);
+    }
+
+    private final By itemUnitsTab = By.xpath("//li[@id=\"ItemUnits\"]");
+    private final By isActivatedToggle = By.xpath("//input[@id='IsActivated']");
+
+    @Step("Navigate to Item Units tab")
+    public ItemUnitsPage navigateToItemUnitsTab() {
+        commons.navigateToSystemAdministrationModule();
+        driver.element().click(itemUnitsTab);
+        return this;
+    }
+
+    @Step("Click on Add Item Unit button")
+    public ItemUnitsPage clickOnAddItemUnitButton() {
+        commons.clickOnAddBtn();
+        return this;
+    }
+
+    @Step("Fill Arabic and English Name")
+    public ItemUnitsPage fillNameFields(String nameAR, String nameEN) {
+        commons.fillNameFields(nameAR, nameEN);
+        return this;
+    }
+
+    @Step("Toggle Is Activated")
+    public ItemUnitsPage toggleIsActivated(boolean isActivated) {
+        if(isActivated && !driver.element().isSelected(isActivatedToggle)) {
+            driver.element().click(isActivatedToggle);
+        } else if(!isActivated && driver.element().isSelected(isActivatedToggle)) {
+            driver.element().click(isActivatedToggle);
+        }
+        return this;
+    }
+
+    @Step("Click on Save button")
+    public ItemUnitsPage clickOnSaveBtn() {
+        commons.clickOnSaveBtn();
+        return this;
+    }
+
+    @Step("check if the item unit is added successfully")
+    public ItemUnitsPage checkSuccessMessageAndClickOk() {
+        commons.checkSuccessMessageAndClickOk();
+        return this;
+    }
+
+    @Step("Create new item unit")
+    public ItemUnitsPage createNewItemUnit(String nameAR, String nameEN, boolean isActivated) {
+        navigateToItemUnitsTab()
+                .clickOnAddItemUnitButton()
+                .fillNameFields(nameAR, nameEN)
+                .toggleIsActivated(isActivated)
+                .clickOnSaveBtn()
+                .checkSuccessMessageAndClickOk();
+        return this;
+    }
+
+    @Step("Edit item unit")
+    public ItemUnitsPage updateItemUnit(String nameAR, String nameEN, boolean isActivated) {
+        navigateToItemUnitsTab();
+        commons.goToUpdateForm();
+                fillNameFields(nameAR, nameEN)
+                .toggleIsActivated(isActivated)
+                .clickOnSaveBtn()
+                .checkSuccessMessageAndClickOk();
+        return this;
+    }
+
+    @Step("Delete item unit")
+    public ItemUnitsPage deleteItemUnit() {
+        commons.delete();
+        return this;
+    }
 }
--- Matgry/src/test/java/tests/systemAdministrationModuleTest/ItemUnitsTest.java
+++ Matgry/src/test/java/tests/systemAdministrationModuleTest/ItemUnitsTest.java
@@ -1,4 +1,62 @@
 package tests.systemAdministrationModuleTest;
 
+import driverFactory.Driver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.systemAdministration.ItemUnitsPage;
+import utilities.DataReader;
+
 public class ItemUnitsTest {
+
+    public Driver driver;
+    public ItemUnitsPage itemUnitsPage;
+    String credentialsJsonFilePath = "src/test/resources/testData/credentials.json";
+    public String itemUnitsJsonFilePath = "src/test/resources/testData/systemAdministrationData/itemUnitsData.json";
+
+    @BeforeClass
+    public void setUp() {
+        driver = new Driver();
+        itemUnitsPage = new ItemUnitsPage(driver);
+        DataReader.loadFiles(credentialsJsonFilePath);
+        DataReader.loadFiles(itemUnitsJsonFilePath);
+        String username = DataReader.getValue(credentialsJsonFilePath, "username");
+        String password = DataReader.getValue(credentialsJsonFilePath, "password");
+
+        new LoginPage(driver)
+                .fillUserNameFiled(username)
+                .fillPasswordField(password)
+                .clickLoginBtn();
+    }
+
+    @Test(priority = 1)
+    public void createItemUnit() {
+        itemUnitsPage
+                .createNewItemUnit(
+                        DataReader.getValue(itemUnitsJsonFilePath, "create.nameAR"),
+                        DataReader.getValue(itemUnitsJsonFilePath, "create.nameEN"),
+                        Boolean.parseBoolean(DataReader.getValue(itemUnitsJsonFilePath, "create.isActivated"))
+                );
+    }
+    @Test(priority = 2)
+    public void editItemUnit() {
+        itemUnitsPage
+                .updateItemUnit(
+                        DataReader.getValue(itemUnitsJsonFilePath, "update.nameAR"),
+                        DataReader.getValue(itemUnitsJsonFilePath, "update.nameEN"),
+                        Boolean.parseBoolean(DataReader.getValue(itemUnitsJsonFilePath, "update.isActivated"))
+                );
+    }
+
+    @Test(priority = 3)
+    public void deleteItemUnit() {
+        itemUnitsPage.deleteItemUnit();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.browser().deleteAllCookies();
+        driver.quit();
+    }
 }
